33df789fe82133dc93b2c2b99011493f1c8ffe67
Abhijith <83421731+abhijithb2109@users.noreply.github.com>
2022-12-31 09:48:27 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -91,7 +91,7 @@ async def start(client, message):
             InlineKeyboardButton("➕️𝗔𝗗𝗗 𝗠𝗘 𝗧𝗢 𝗨𝗥 𝗚𝗥𝗢𝗨𝗣𝗦➕️", url=f"http://t.me/{temp.U_NAME}?startgroup=true")
             ],[
             InlineKeyboardButton("𝗦𝗘𝗔𝗥𝗖𝗛🔍", switch_inline_query_current_chat=''), 
-            InlineKeyboardButton("𝗨𝗣𝗗𝗔𝗧𝗘𝗦", url="https://t.me/Astra_botz")
+            InlineKeyboardButton("𝗨𝗣𝗗𝗔𝗧𝗘𝗦", url="https://t.me/moviematrix2")
             ],[      
             InlineKeyboardButton("𝗛𝗘𝗟𝗣", callback_data="help"),
             InlineKeyboardButton("𝗔𝗕𝗢𝗨𝗧", callback_data="about")
